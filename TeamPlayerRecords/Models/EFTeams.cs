a2a8e69703602ca319130983c773475f38da2da1
HarleenSingh <harleensingh315@gmail.com>
2018-11-28 09:02:29 -0500

    Assignment 2 Part 2 Commit 1

---
+++ TeamPlayerRecords/Models/EFTeams.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamPlayerRecords.Models
+{
+    public class EFTeams : ITeamMock
+    {
+        private Model1 db = new Model1();
+
+        public IQueryable<Team> Teams { get { return db.Teams; } }
+
+
+        public void Delete(Team team)
+        {
+            db.Teams.Remove(team);
+            db.SaveChanges();
+
+        }
+
+        public Team Save(Team team)
+        {
+            if (team.ID == 0)
+            {
+                db.Teams.Add(team);
+            }
+            else
+            {
+                db.Entry(team).State = System.Data.Entity.EntityState.Modified;
+            }
+            db.SaveChanges();
+            return team;
+        }
+    }
+}
