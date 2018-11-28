2f0cbbb55d8135b69ab30091745bb76dbe7aecd7
HarleenSingh <harleensingh315@gmail.com>
2018-11-28 13:18:16 -0500

    Assignment 2 Part 2 Commit 6 - Final

--- TeamPlayerRecords.Tests/Controllers/TeamControllerTest.cs
+++ TeamPlayerRecords.Tests/Controllers/TeamControllerTest.cs
@@ -80,6 +80,149 @@ namespace TeamPlayerRecords.Tests.Controllers
         }
         #endregion
 
+        #region Test Cases For Edit Teams - GET & POST 
 
+        #region Test Case Section For GET: /Team/Edit
+        [TestMethod]
+        public void Edit_NoId()
+        {
+            int? id = null;
+            var result = (ViewResult)controller.Edit(id);
+            Assert.AreEqual("Error", result.ViewName);
+        }
+        [TestMethod]
+        public void Edit_ViewLoads()
+        {
+            ViewResult actual = (ViewResult)controller.Edit(100);
+            Assert.AreEqual("Edit", actual.ViewName);
+        }
+        [TestMethod]
+        public void Edit_LoadsTeams()
+        {
+            Team actual = (Team)((ViewResult)controller.Edit(100)).Model;
+            Assert.AreEqual(teams[0], actual);
+        }
+        #endregion
+
+        #region Test Cases For POST: Team/Edit - Saved Edited Team Details
+        [TestMethod]
+        public void EditPostTeam_LoadsIndex()
+        {
+            RedirectToRouteResult result = (RedirectToRouteResult)controller.Edit(teams[0]);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void EditPost_InvalidLoadView()
+        {
+            Team id = new Team { ID = 201 };
+            controller.ModelState.AddModelError("Error", "It'll Not Save");
+            ViewResult result = (ViewResult)controller.Edit(id);
+            Assert.AreEqual("Edit", result.ViewName);
+        }
+
+        [TestMethod]
+        public void EditPost_InvalidLoadTeam()
+        {
+            Team id = new Team { ID = 101 };
+            controller.ModelState.AddModelError("Error", "It'll Not Save");
+            Team result = (Team)((ViewResult)controller.Edit(id)).Model;
+            Assert.AreEqual(id, result);
+        }
+        #endregion
+
+        #endregion
+
+        #region Test Cases For Add New Team Get & POST
+
+        #region Test Cases For GET: /Team/Create - Add New Team
+        [TestMethod]
+        public void Create_ViewLoads()
+        {
+            var result = (ViewResult)controller.Create();
+            Assert.AreEqual("Create", result.ViewName);
+        }
+        #endregion
+
+        #region Test Cases For POST: /Team/Create - Add New Team POST Save Team Details
+        [TestMethod]
+        public void Create_ValidTeam()
+        {
+            Team newTeam = new Team
+            {
+                ID = 300,
+                TeamName = "England",
+                CoachName = "Steve Waugh",
+                Ratings = 3
+            };
+            RedirectToRouteResult result = (RedirectToRouteResult)controller.Create(newTeam);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void Create_InValidTeam()
+        {
+            Team newTeam = new Team();
+            controller.ModelState.AddModelError("Unable To Create Team", "Team Creation Exception");
+            ViewResult result = (ViewResult)controller.Create(newTeam);
+            Assert.AreEqual("Create", result.ViewName);
+        }
+        #endregion
+
+        #endregion
+
+        #region Test Cases For Delete Team - Get & Post
+
+        #region Test Cases For GET: /Team/Delete - Delete Team
+        [TestMethod]
+        public void Delete_NoId()
+        {
+            var result = (ViewResult)controller.Delete(null);
+            Assert.AreEqual("Error", result.ViewName);
+        }
+        [TestMethod]
+        public void Delete_InvalidId()
+        {
+            var result = (ViewResult)controller.Delete(101);
+            Assert.AreEqual("Error", result.ViewName);
+        }
+        [TestMethod]
+        public void Delete_ValidId_LoadsView()
+        {
+            var result = (ViewResult)controller.Delete(100);
+            Assert.AreEqual("Delete", result.ViewName);
+        }
+        [TestMethod]
+        public void Delete_ValidId_LoadsModel()
+        {
+            Team actual = (Team)((ViewResult)controller.Delete(100)).Model;
+            Assert.AreEqual(teams[0], actual);
+        }
+        #endregion
+
+        #region Test Case For POST: /Team/DeleteConfirmed - POST Team Delete Confirmation
+        [TestMethod]
+        public void Delete_Confirmed_NoId()
+        {
+            ViewResult result = (ViewResult)controller.DeleteConfirmed(null);
+            Assert.AreEqual("Error", result.ViewName);
+        }
+
+        [TestMethod]
+        public void Delete_Confirmed_InvalidId()
+        {
+            ViewResult result = (ViewResult)controller.DeleteConfirmed(201);
+            Assert.AreEqual("Error", result.ViewName);
+        }
+
+        [TestMethod]
+        public void Delete_Confirmed_validId()
+        {
+            RedirectToRouteResult result = (RedirectToRouteResult)controller.DeleteConfirmed(100);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+        }
+        #endregion
+
+        #endregion
     }
 }
